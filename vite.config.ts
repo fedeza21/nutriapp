49473e879846ee1452bb04c041a889684466c17d
fedeza21 <dmze21@gmail.com>
2026-01-02 03:37:03 -0300

    11

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  define: {
-    // Esto inyecta la variable de entorno durante el build de Vercel
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/', // 🔥 VERCEL
 });
